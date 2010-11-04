84ae8eea2b147a6b1d891e986b4ff86aa194a8f1
moishel <moishel@11c33a9b-5245-0769-e515-986bba7ed88e>
2010-11-04 00:15:59 +0000

    Initial checkin of Java Channel API demo.

---
+++ trunk/src/com/google/appengine/demos/channeltactoe/OpenedServlet.java
@@ -0,0 +1,40 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package com.google.appengine.demos.channeltactoe;
+
+import com.google.appengine.api.channel.ChannelMessage;
+import com.google.appengine.api.channel.ChannelService;
+import com.google.appengine.api.channel.ChannelServiceFactory;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author moishel@google.com (Your Name Here)
+ *
+ */
+public class OpenedServlet extends HttpServlet {
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    String gameId = req.getParameter("g"); 
+    PersistenceManager pm = PMF.get().getPersistenceManager();
+    Game game = pm.getObjectById(Game.class, KeyFactory.stringToKey(gameId));
+    if (gameId != null && req.getUserPrincipal() != null) {
+      String channelKey = userService.getCurrentUser().getUserId() + gameId;
+      ChannelService channelService = ChannelServiceFactory.getChannelService();
+      channelService.sendMessage(new ChannelMessage(channelKey, game.getMessageString()));
+      resp.setContentType("text/plain");
+      resp.getWriter().println("ok");
+    } else {
+      resp.setStatus(401);
+    }
+  }
+}
