807f471bc1c12001544beeb80a6cf73aa5fdcaf3
moishel <moishel@11c33a9b-5245-0769-e515-986bba7ed88e>
2010-11-06 00:30:43 +0000

    Trying to fix css up.

--- trunk/src/com/google/appengine/demos/channeltactoe/TicTacToeServlet.java
+++ trunk/src/com/google/appengine/demos/channeltactoe/TicTacToeServlet.java
@@ -17,13 +17,39 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class TicTacToeServlet extends HttpServlet {
+  private String getGameUriWithGameParam(HttpServletRequest req,
+      String gameKey) throws IOException {    
+    try {
+      String query;
+      if (gameKey == null) {
+        query = "";
+      } else {
+        query = "g=" + gameKey;
+      }
+      URI thisUri = new URI(req.getRequestURL().toString());
+      URI uriWithOptionalGameParam = new URI(thisUri.getScheme(),
+          thisUri.getUserInfo(),
+          thisUri.getHost(),
+          thisUri.getPort(),
+          thisUri.getPath(),
+          query,
+          "");
+      return uriWithOptionalGameParam.toString();
+    } catch (URISyntaxException e) {
+      throw new IOException(e.getMessage());
+    }
+    
+  }
+  
   @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {    
+    final UserService userService = UserServiceFactory.getUserService();
+    final URI uriWithOptionalGameParam; 
+    String gameKey = req.getParameter("g");
     if (userService.getCurrentUser() == null) {
       String thisURL = req.getRequestURL().toString();      
       resp.getWriter().println("<p>Please <a href=\"" +
-          userService.createLoginURL(thisURL) + "\">sign in</a>.</p>");
+          userService.createLoginURL(getGameUriWithGameParam(req, gameKey)) + "\">sign in</a>.</p>");
       
       return;
     }
@@ -31,11 +57,10 @@ public class TicTacToeServlet extends HttpServlet {
     PersistenceManager pm = PMF.get().getPersistenceManager();
     
     Game game = null;
-    String gameKey = req.getParameter("g");
     String userId = userService.getCurrentUser().getUserId();
     if (gameKey != null) {
       game = pm.getObjectById(Game.class, KeyFactory.stringToKey(gameKey));
-      if (game.getUserO() == null && userId != game.getUserX()) {
+      if (game.getUserO() == null && !userId.equals(game.getUserX())) {
         game.setUserO(userId);
       }
     } else {
@@ -47,34 +72,18 @@ public class TicTacToeServlet extends HttpServlet {
     
     ChannelService channelService = ChannelServiceFactory.getChannelService();
     String token = channelService.createChannel(game.getChannelKey(userId));
+
+    FileReader reader = new FileReader("index-template");
+    CharBuffer buffer = CharBuffer.allocate(16384);
+    reader.read(buffer);
+    String index = new String(buffer.array());
+    index = index.replaceAll("\\{\\{ game_key \\}\\}", gameKey);
+    index = index.replaceAll("\\{\\{ me \\}\\}", userId);
+    index = index.replaceAll("\\{\\{ token \\}\\}", token);
+    index = index.replaceAll("\\{\\{ initial_message \\}\\}", game.getMessageString());
+    index = index.replaceAll("\\{\\{ game_link \\}\\}", getGameUriWithGameParam(req, gameKey));
     
-    try {
-      URI thisUri = new URI(req.getRequestURL().toString());
-      URI qualifiedUri = new URI(thisUri.getScheme(),
-                                 thisUri.getUserInfo(),
-                                 thisUri.getHost(),
-                                 thisUri.getPort(),
-                                 thisUri.getPath(),
-                                 "g=" + gameKey,
-                                 "");
-
-      FileReader reader = new FileReader("index-template");
-      CharBuffer buffer = CharBuffer.allocate(16384);
-      reader.read(buffer);
-      String index = new String(buffer.array());
-      index = index.replaceAll("\\{\\{ game_key \\}\\}", gameKey);
-      index = index.replaceAll("\\{\\{ me \\}\\}", userId);
-      index = index.replaceAll("\\{\\{ token \\}\\}", token);
-      index = index.replaceAll("\\{\\{ initial_message \\}\\}", game.getMessageString());
-      index = index.replaceAll("\\{\\{ game_link \\}\\}", qualifiedUri.toString());
-      
-      resp.setContentType("text/html");
-      resp.getWriter().write(index);
-    } catch (URISyntaxException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }    
-
-
+    resp.setContentType("text/html");
+    resp.getWriter().write(index);
   }
 }
